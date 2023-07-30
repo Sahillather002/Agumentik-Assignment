f5bc92ad1eb8c3faf921d3ec6a19eaf16dd1406f
Sahil Lather <sahillather002@gmail.com>
2023-07-30 13:27:54 +0530

    navbar done

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import './Header.css'
+import babe from '../Images/front.png'
 const Header = () => {
   return (
     <div>
+        
         <div className='head'>
             hello
         </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,21 +1,45 @@
 import React from 'react';
 import './Navbar.css'; // Import your CSS file for styling
-
+import logo from '../Images/promaLogo.png';
+import {AiOutlineArrowRight} from "react-icons/ai"
 const Navbar = () => {
   return (
     <nav className="beautiful-navbar">
       <div className="navbar-container">
-        <div className="brand-logo">
-          {/* Add your logo or brand name here */}
-          <a href="/">Logo</a>
+        <div>
+          <img src={logo} alt="My logo"></img>
         </div>
         <ul className="nav-menu">
-          <li><a href="/">Home</a></li>
+          <li ><a href="/">Home</a></li>
           <li><a href="/services">Product</a></li>
           <li><a href="/portfolio">Features</a></li>
           <li><a href="/about">Membership</a></li>
           <li><a href="/contact">Company</a></li>
         </ul>
+      </div>
+      <button className='navButtons'>
+        World Class Creatives
+      </button>
+      <div className='navHead'>
+        <h1>
+            JOIN OUR MEMBERSHIP
+        </h1>
+        <h1>
+            AND START PROJECT
+        </h1>
+        <p className='navP'>
+            A membership program is a marketing startegy used by companies to reward their 
+            {'\n'}customers for their loyalty and repeat business.Joining a membership program usually
+            involves signing up and paying a fee or fulfilling certain criteria.
+        </p>
+      </div>
+      <div>
+        <button className='navButton'>
+            <span>Lets go</span>
+            <span>
+                <AiOutlineArrowRight/>
+            </span>
+        </button>
       </div>
     </nav>
   );
